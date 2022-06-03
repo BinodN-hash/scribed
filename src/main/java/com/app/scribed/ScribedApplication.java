63cc513c283281c145f75c3c0abe974f8cfac3ef
Binod Nakhan <binodnakhan21@gmail.com>
2022-06-03 16:14:19 +0545

    project setup

---
+++ src/main/java/com/app/scribed/ScribedApplication.java
@@ -0,0 +1,13 @@
+package com.app.scribed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScribedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScribedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/scribed/ScribedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.scribed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScribedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
